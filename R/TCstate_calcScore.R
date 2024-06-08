1fbc53d79fe4421c4912fc1a2fdee054156a31e2
JingMin Yang <78607522+VyvyanYjm@users.noreply.github.com>
2024-06-08 12:09:33 +0800

    Update TCstate_calcScore.R

--- R/TCstate_calcScore.R
+++ R/TCstate_calcScore.R
@@ -23,7 +23,9 @@ TCstate_calcScore <- function (object, reference = ref_data, nbin=50, ctrl = 100
             message(paste0("Calculating of ", "Helper"), " state")
         } else if (k %in% c(1,2,3,7,8,9,10)) {
             message(paste0("Calculating of ", names(markers)[k]), " state")
-        } else {}
+        } else if (k %in% c(5,6)){
+            # 不显示
+        }
         features <- markers[[k]]
         missing_features_input_sample <- setdiff(features, rownames(all_sample_TPM1))
         if (length(missing_features_input_sample) > 0) {
