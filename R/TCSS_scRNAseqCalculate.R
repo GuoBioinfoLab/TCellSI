a0c2790ff9ef4191159c5566c69ec832b5784952
JingMin Yang <78607522+VyvyanYjm@users.noreply.github.com>
2024-11-08 13:16:08 +0800

    Update TCSS_scRNAseqCalculate.R

--- R/TCSS_scRNAseqCalculate.R
+++ R/TCSS_scRNAseqCalculate.R
@@ -101,12 +101,10 @@ TCSS_scRNAseqCalculate <- function(object, reference = ref_data, ref = TRUE, nbi
       features_ref <- reference[features, names(markers)[k]]
       div_percent <- features_sample / features_ref
       flag_lower <- (features_sample - features_ref) < 0
-      number_lower <- colSums(flag_lower)
-
+      number_lower <- colSums(flag_lower) 
       percentage <- sapply(seq_len(ncol(div_percent)), function(i) {
-        ifelse(number_lower[i] == 0, 1, 
-               sum(div_percent[, i][flag_lower[, i]], 
-                   (length(flag_lower[, i]) - sum(flag_lower[, i]))) / length(features))
+      ifelse(number_lower[i] == 0, 1,
+             (sum(div_percent[, i][flag_lower[, i]]) + sum(flag_lower[, i] == FALSE)) / length(features))
       })
       features.scores.vec <- features.scores.vec * percentage
     }
